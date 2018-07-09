20d02b08ebc5f0224b9ce89abd8b015b27fd2b35
rahulkumaran <rahuldravid313@gmail.com>
2018-07-09 22:36:08 +0530

    final result of pseudocode in sudocode.txt is here

--- sudocode.c
+++ sudocode.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int main()
 {
-float i=0;
-int k=10;
-for(int i=0; i <= 10; i++)
-{
-printf("hey dude ")
-}
+	float i=0;
+	int k=100;
+	for(int i=0; i <= 10; i++)
+	{
+		printf("hey dude\n");
+		k++;
+		k+=100;
+	}
+	for(int i=0; i <= 10; i+=2)
+	{
+		while(i<=5)
+		{
+			printf("in loop 1\n");
+			printf("wooho\n");
+			i++;
+		}
+		printf("success\n");
+		k++;
+	}
+	while(i<=10)
+	{
+		printf("here\n");
+		printf("there\n");
+		printf("everywhere\n");
+		i++;
+	}
+	printf("It's over mama");
 }
