51cb8c84973e676a20ab73da492d70fd15d37dd4
rahulkumaran <rahuldravid313@gmail.com>
2018-07-09 17:07:02 +0530

    added functionality for for

--- app.py
+++ app.py
@@ -3,18 +3,41 @@ if(__name__=='__main__'):
 	filename = "sudocode.txt"
 	file_ptr = open(filename, "r")
 	code_file_ptr = open(filename[0:len(filename)-4]+".c", "w")
-	code_file_ptr.write("#include <stdio.h>\n#include <stdlib.h>\nint main()\n{\n")
+	code_file_ptr.write("#include <stdio.h>\n\n#include <stdlib.h>\nint main()\n{\n")
+	no = 0
 	for line in file_ptr:
+		no+=1
 		line_elem = line.split(" ")
 		line_elem[-1] = line_elem[-1].replace("\n","")
 		print(line_elem)
-		print("int" in line_elem)
+		print("For" in line_elem)
 		line_of_code = ""
-		if(("int" not in line_elem) and ("float" not in line_elem) and ("Initialise" in line)):
+		if(("Initialise" in line_elem) and ("int" not in line_elem) and ("float" not in line_elem)):
+			print(no)
 			line_of_code += "float " + line_elem[1]  + ";"
-		if(("Initialise" in line) and (("int" in line_elem) or ("float" in line_elem))):
+		if(("Initialise" in line_elem) and (("int" in line_elem) or ("float" in line_elem))):
+			print(no)
 			line_of_code += line_elem[1] + " " + line_elem[2]  + ";"
 
+		if(("For" in line_elem) and ("gap" not in line_elem)):
+			if(int(line_elem[2][-1]) < int(line_elem[4])):
+				line_of_code += "for(int " + line_elem[2] + "; " + line_elem[2][0] + " <= " + line_elem[4] + "; " + line_elem[2][0] + "++)\n{"
+				print(line_of_code)
+			else:
+				line_of_code += "for(int " + line_elem[2] + "; " + line_elem[2][0] + " >= " + line_elem[4] + "; " + line_elem[2][1] + "--)\n{"
+				print(line_of_code)
+
+		
+		if("Endfor" in line):
+				print(no)
+				line_of_code += "}"
+
+		if("Print" in line):
+				line_of_code += "printf(\""
+				for i in range(1,len(line_elem)):
+					line_of_code += line_elem[i] + " "
+				line_of_code += "\")"
+		
 		code_file_ptr.write(line_of_code+'\n')
 
 	code_file_ptr.write("}")
