5dbfcf16d72bcc165d9808d5c35a5a936fe0ed37
rahulkumaran <rahuldravid313@gmail.com>
2018-07-23 00:11:32 +0530

    updated test file name

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from libs.cleaner import code_cleaner, code_execute
 
 if(__name__=='__main__'):
 	
-	filename = "testfiles/sudocode2.txt"
+	filename = "testfiles/sudocode.txt"
 
 	get_code(filename)
 
