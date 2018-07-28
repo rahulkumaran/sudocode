c6598a1b602e62d1da4e8b826f0195cb9467fb89
rahulkumaran <rahuldravid313@gmail.com>
2018-07-28 13:01:59 +0530

    added __name__ print

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from libs.sudocode import get_code
 from libs.cleaner import code_cleaner, code_execute
 
 if(__name__=='__main__'):
-	
+
+	#print(__name__)
 	filename = "testfiles/sudocode3.txt"
 
 	get_code(filename)
@@ -11,4 +12,4 @@ if(__name__=='__main__'):
 
 	code_execute(filename[0:len(filename)-4]+".c")
 
-	print(__name__)
+	#print(__name__)
