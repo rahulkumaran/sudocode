cf0dce5c8c7fe44eda0a00654396a2c8dd795101
pprabh2007 <32813957+pprabh2007@users.noreply.github.com>
2018-12-06 02:07:06 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from libs.sudocode import get_code
 from libs.cleaner import code_cleaner, code_execute
+import sys
+usage="USAGE: python main.py <name_of_file>"
+
+
+if(len(sys.argv)==1):
+	print("Error: Too few arguments")
+	print(usage)
+	sys.exit(0)
+elif (len(sys.argv)>2):
+	print("Error: Too many arguments")
+	print(usage)
+	sys.exit(0)
 
 if(__name__=='__main__'):
 
 	#print(__name__)
-	filename = "testfiles/sudocode3.txt"
+	filename = sys.argv[1]
 
 	get_code(filename)
 
