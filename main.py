227773ddc04dce714ac3b8dda304670951801c97
ayush <atulg7516@gmail.com>
2018-12-06 12:50:38 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from libs.sudocode import get_code
 from libs.cleaner import code_cleaner, code_execute
 
-if(__name__=='__main__'):
-
-	#print(__name__)
-	filename = "testfiles/sudocode3.txt"
+if __name__=='__main__':
+	filename = "testfiles/sudocode2.txt"
 
 	get_code(filename)
 
 	code_cleaner(filename[0:len(filename)-4]+".c")
 
 	code_execute(filename[0:len(filename)-4]+".c")
-
-	#print(__name__)
