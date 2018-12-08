6a4c0e067b22308753e208cee51e4febc5560f02
ayush <atulg7516@gmail.com>
2018-12-08 15:29:43 +0530

    updated for python

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-from libs.sudocode_c import get_code
-from libs.cleaner import code_cleaner, code_execute
+from libs import sudocode_c
+from libs import sudocode_py
+from libs import cleaner
 import sys
 
 if(len(sys.argv) is 2):
 	if(__name__=='__main__'):
 
 		#print(__name__)
+		print("In which language do you want to convert pseudo code, python or c ?")
+		user_input = str(input('python or c \n'))
 		filename = sys.argv[1]
 
-		get_code(filename)
+		if user_input == 'c':
+			sudocode_c.get_code(filename)
+			cleaner.code_cleaner(filename[0:len(filename)-4]+".c")
+			cleaner.code_execute(filename[0:len(filename)-4]+".c")
 
-		code_cleaner(filename[0:len(filename)-4]+".c")
-
-		code_execute(filename[0:len(filename)-4]+".c")
+		if user_input == 'python':
+			sudocode_py.get_code(filename)
+			cleaner.code_execute(filename[0:len(filename)-4]+'.py')
+			print('f')
 
 		#print(__name__)
 
