3a8d58982b508daf2da028c8743390f98046bb90
ayush <atulg7516@gmail.com>
2018-12-08 15:47:31 +0530

    print function added

--- libs/sudocode_py.py
+++ libs/sudocode_py.py
@@ -9,4 +9,7 @@ def get_code(filename):
         if 'variable' in line_element:
             line_of_code = line_element[1] + '\n'
 
+        elif 'print' in line_element:
+            line_of_code = line_element[0] + '(' + "\"" + str(' '.join(line_element[1:])) + "\"" + ')'
+
         python_code.write(line_of_code)
