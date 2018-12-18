b2436ceb1149fb373e0a3bb6e1d73cde5c7bce29
Rahul Arulkumaran <rahuldravid313@gmail.com>
2018-12-18 23:41:46 +0530

    Update web-dev branch (#66)
    * Rename Contribute_guide.md to CONTRIBUTION.md
    
    * Create .travis.yml
    
    * Create requirements.txt
    
    * Update .travis.yml
    
    * Update README.md  (#44)
    
    * Update README.md
    
    * Rename CONTRIBUTION.md to CONTRIBUTING.md
    
    * Rename .github/ISSUE_TEMPLATE/pull_request_template.md to .github/pull_request_template.md
    
    * Code of conduct.md (#53)
    
    Add Code of Conduct
    
    * Pr38 (#55)
    
    * capture returned value
    
    * corrections in sudocode_c.py (#58)
    
    * conflicts resolved
    
    * Necessary Badges Added  (#62)
    
    * Add .DS_Store to .gitignore
    
    * Update README.md
    
    Tags Added including link to the communication channel
    
    * Update README.md
    
    Commited in wrong project , Sorry
    
    * Update README.md
    
    * Update README.md
    
    Added PR and Issue Badge showing number of open PRs and issues at the moment respectively.
    Added KWoC 2018 Badge
    
    * Ar786 patch 1 (#63)
    
    * testfiles for the python conversion
    
    * python code conversion- Initialize logic
    
    * Added license badge (#64)

---
+++ testfiles/sudocode_py2.py
@@ -0,0 +1,40 @@
+def test(a,b,c):
+
+	print("test func\n")
+	a++
+	b++
+	c++
+	a = b + c
+	return a
+
+	
+
+
+
+i=0
+k=100
+for i in range(0,11):
+
+	print("hey dude\n")
+	k++
+	k+=100
+	
+for i in range(0,11,2):
+
+	while i<=5:
+
+		print("in loop 1\n")
+		print("wooho\n")
+		i++
+		
+	print("success\n")
+	k++
+	
+while i<=10:
+
+	print("here\n")
+	print("there\n")
+	print("everywhere\n")
+	i++
+	
+print("It's over mama\n")
---
+++ testfiles/sudocode_py3.py
@@ -0,0 +1,28 @@
+def test_3(a,temp,test):
+
+
+	b=10
+	test=10
+	a = b/test
+	return a
+
+	
+
+
+
+a=10
+hi=20
+print("{}".format(hi))
+test_3(0,10,10)
+for i in range(0,11):
+
+	if(i%2==0):
+
+		print("{}".format(i))
+	else:
+
+		print("not even\n")
+		
+
+	
+print("{}".format(hi))
